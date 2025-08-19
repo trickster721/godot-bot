e507e097ac213261ecd032c547a6629ebdada13a
trickster721 <trickster721@gmail.com>
2025-08-19 11:38:50 -0400

    implemented blog posts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,139 +1,144 @@
 import express from "express";
 import {
-  InitResponse,
-  IncrementResponse,
-  DecrementResponse,
-} from "../shared/types/api";
-import {
-  createServer,
-  context,
-  getServerPort,
-  reddit,
-  redis,
+	createServer,
+	context,
+	getServerPort,
+	reddit,
+//	redis,
+	Post,
+	settings
 } from "@devvit/web/server";
-import { createPost } from "./core/post";
 
 const app = express();
 
 // Middleware for JSON body parsing
 app.use(express.json());
 // Middleware for URL-encoded body parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 // Middleware for plain text body parsing
 app.use(express.text());
 
 const router = express.Router();
 
-router.get<
-  { postId: string },
-  InitResponse | { status: string; message: string }
->("/api/init", async (_req, res): Promise<void> => {
-  const { postId } = context;
+router.post("/internal/scheduler/check-for-blog-post", async (_req, res): Promise<void> => {
 
-  if (!postId) {
-    console.error("API Init Error: postId not found in devvit context");
-    res.status(400).json({
-      status: "error",
-      message: "postId is required but missing from context",
-    });
-    return;
-  }
+		console.log(`Checking for new blog post at ${new Date().toTimeString()}`);
+		const {subredditName} = context;
 
-  try {
-    const [count, username] = await Promise.all([
-      redis.get("count"),
-      reddit.getCurrentUsername(),
-    ]);
+		const response = await fetch('https://godotengine.org/rss.xml', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/rss+xml',
+			},
+		});
 
-    res.json({
-      type: "init",
-      postId: postId,
-      count: count ? parseInt(count) : 0,
-      username: username ?? "anonymous",
-    });
-  } catch (error) {
-    console.error(`API Init Error for post ${postId}:`, error);
-    let errorMessage = "Unknown error during initialization";
-    if (error instanceof Error) {
-      errorMessage = `Initialization failed: ${error.message}`;
-    }
-    res.status(400).json({ status: "error", message: errorMessage });
-  }
-});
+		const xml_data = await response.text();
 
-router.post<
-  { postId: string },
-  IncrementResponse | { status: string; message: string },
-  unknown
->("/api/increment", async (_req, res): Promise<void> => {
-  const { postId } = context;
-  if (!postId) {
-    res.status(400).json({
-      status: "error",
-      message: "postId is required",
-    });
-    return;
-  }
+		const rss_items = Array.from(xml_data.matchAll(/(?<=<item>)(.*?)(?=<\/item>)/gs));
+		if (rss_items.length > 6) {rss_items.length = 6;}
+		console.log(`found ${rss_items.length} rss items`);
 
-  res.json({
-    count: await redis.incrBy("count", 1),
-    postId,
-    type: "increment",
-  });
-});
+		const users = ["godot-bot", "GodotTeam"];
 
-router.post<
-  { postId: string },
-  DecrementResponse | { status: string; message: string },
-  unknown
->("/api/decrement", async (_req, res): Promise<void> => {
-  const { postId } = context;
-  if (!postId) {
-    res.status(400).json({
-      status: "error",
-      message: "postId is required",
-    });
-    return;
-  }
+		let existing_posts: Post[] = [];
 
-  res.json({
-    count: await redis.incrBy("count", -1),
-    postId,
-    type: "decrement",
-  });
-});
+		for (let user of users)
+		{
+			const more_posts: Post[] = await reddit.getPostsByUser({
+				username: user,
+				limit: 12,
+				sort: "new",
+				timeframe: "all"
+			}).all();
 
-router.post("/internal/on-app-install", async (_req, res): Promise<void> => {
-  try {
-    const post = await createPost();
+			existing_posts = existing_posts.concat(more_posts);
+		}
 
-    res.json({
-      status: "success",
-      message: `Post created in subreddit ${context.subredditName} with id ${post.id}`,
-    });
-  } catch (error) {
-    console.error(`Error creating post: ${error}`);
-    res.status(400).json({
-      status: "error",
-      message: "Failed to create post",
-    });
-  }
-});
+		console.log(`found ${existing_posts.length} recent posts`);
 
-router.post("/internal/menu/post-create", async (_req, res): Promise<void> => {
-  try {
-    const post = await createPost();
+		const post_index = {};
+		let posts_to_stick: Post[] = [];
 
-    res.json({
-      navigateTo: `https://reddit.com/r/${context.subredditName}/comments/${post.id}`,
-    });
-  } catch (error) {
-    console.error(`Error creating post: ${error}`);
-    res.status(400).json({
-      status: "error",
-      message: "Failed to create post",
-    });
-  }
+		for (let p of existing_posts)
+		{
+			if (p.subredditName === subredditName && !p.removed && !p.hidden)
+			{
+				post_index[p.url] = p;
+			}
+		}
+
+		const posts_to_add = [];
+
+		for (let i in rss_items)
+		{
+			const link = rss_items[i][0].match(/(?<=<link>)(.*?)(?=<\/link>)/g)[0]
+			if (post_index.hasOwnProperty(link))
+			{
+				posts_to_stick[i] = post_index[link];
+			}
+			else
+			{
+				posts_to_add.push({
+					url: link,
+					title: rss_items[i][0].match(/(?<=<title>)(.*?)(?=<\/title>)/g)[0],
+					summary: rss_items[i][0].match(/(?<=<summary>)(.*?)(?=<\/summary)/g)[0],
+					category: rss_items[i][0].match(/(?<=<category>)(.*?)(?=<\/category>)/g)[0],
+					sticky_pos: i
+				});
+				console.log(`found new post: ${link}`)
+			}
+		}
+
+		console.log(`${posts_to_add.length} rss posts were missing`);
+
+		for (let post of posts_to_add)
+		{
+			let flair_text = "";
+			if (post.category === "Release" || post.category === "Pre-release") {flair_text = "official - releases";}
+			else {flair_text = "official - news";}
+
+			const flair_code = await settings.get("flair_code");
+
+			const new_post = await reddit.submitPost({
+				subredditName: subredditName,
+				title: post.title,
+				url: post.url,
+				text: post.summary,
+			//	flairId: "2863c3ce-7cb4-11f0-9fc4-3a7035d1e990", // r/godot_bot_dev
+			//	flairId: "3ea7e314-e209-11ee-875d-863f4dc3d1d4", // r/godot
+				flairId: flair_code,
+				flairText: flair_text
+			});
+
+			posts_to_stick[post.sticky_pos] = new_post;
+		}
+
+		posts_to_stick = posts_to_stick.filter(Boolean);
+
+		for (let post of posts_to_stick) {await post.unsticky();}
+
+		posts_to_stick.reverse();
+
+		console.log(`sticking ${posts_to_stick.length} posts`);
+
+		if (posts_to_stick.length > 0)
+		{
+			const first = posts_to_stick.pop();
+			console.log(`sticking first post: ${first.url}`);
+			await first.sticky(1);
+			for (let post of posts_to_stick)
+				{
+				console.log(`sticking post: ${post.url}`);
+				await post.sticky();
+				}
+		}
+
+	console.log('finished successfully!');
+
+	res.json({
+		showToast: "Done!"
+	});
+
 });
 
 app.use(router);
